e4fca58397459cd1abd03cb919fa24628c63c805
Ilan Olkies <ilanolkies@outlook.com>
2020-03-23 20:45:48 -0300

    Updates

---
+++ precache-manifest.abbb7162333f715a201284b7cae63a76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e7bb7b159fd7644194911ea7c2a1e0",
+    "url": "/rns-addr-sample-app/index.html"
+  },
+  {
+    "revision": "f269cab71e89b7a29948",
+    "url": "/rns-addr-sample-app/static/js/2.2b78fe92.chunk.js"
+  },
+  {
+    "revision": "deea066462e3852fb7f610c3fa67aa20",
+    "url": "/rns-addr-sample-app/static/js/2.2b78fe92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf9a4fc543d24710580",
+    "url": "/rns-addr-sample-app/static/js/main.ee134b84.chunk.js"
+  },
+  {
+    "revision": "b26642e5de5cd340d42a",
+    "url": "/rns-addr-sample-app/static/js/runtime-main.03201730.js"
+  }
+]);
